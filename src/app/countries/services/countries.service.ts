22e2c4f1596924bc1d38476d2b333c874478d05b
Leonel Polanco Fernandez <leonelpolanco2913@gmail.com>
2023-07-14 21:38:14 -0500

    finally
    finally

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -11,65 +11,71 @@ import { Region } from '../interfaces/region.type';
 export class CountriesService {
   private apiURL: string = 'https://restcountries.com/v3.1';
 
-  public chacheStore :  CahceStore = {
+  public chacheStore: CahceStore = {
     byCapital: {
       term: '',
-      countries :[]
+      countries: [],
     },
     byCountries: {
       term: '',
-      countries :[]
+      countries: [],
     },
     byRegion: {
       region: '',
-      countries :[]
+      countries: [],
     },
+  };
+
+  constructor(private http: HttpClient) {
+    this.loadToLocalStorage()
   }
 
-  constructor(private http: HttpClient) {}
-
-  private geCountriesRequest( url : string) : Observable<Country[]>{
-    return this.http.get<Country[]>(url)
-    .pipe(
-      catchError( () => of([])),
-    )
+  private saveToLocalStorage() {
+    localStorage.setItem('cacheStore', JSON.stringify(this.chacheStore));
   }
 
-  searchCountryByAlphaCode( code : string ) : Observable<Country | null> {
+  private loadToLocalStorage() {
+    if(!localStorage.getItem('cacheStore')) return
+
+    this.chacheStore = JSON.parse(localStorage.getItem('cacheStore')!)
+  }
+
+  private geCountriesRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
+
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url = `${this.apiURL}/alpha/${code}`;
-    return this.http.get<Country[]>(url)
-    .pipe(
-      map( countries => countries.length > 0 ?  countries[0] : null),
-      catchError( () => {
-        return of(null)
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+      catchError(() => {
+        return of(null);
       })
-    )
+    );
   }
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiURL}/capital/${term}`;
-    return this.geCountriesRequest(url)
-      .pipe(
-        tap( countries => this.chacheStore.byCapital = { term, countries } ),
-      )
+    return this.geCountriesRequest(url).pipe(
+      tap((countries) => (this.chacheStore.byCapital = { term, countries })),
+      tap( () => this.saveToLocalStorage()),
+    );
   }
 
-  searchCountry(term : string) : Observable<Country[]> {
-    const url = `${this.apiURL}/name/${term}`
-    return this.geCountriesRequest(url)
-    .pipe(
-      tap( countries => this.chacheStore.byCountries = { term, countries } ),
-    )
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiURL}/name/${term}`;
+    return this.geCountriesRequest(url).pipe(
+      tap((countries) => (this.chacheStore.byCountries = { term, countries })),
+      tap( () => this.saveToLocalStorage()),
+    );
   }
 
-  searchRegion( region : Region) : Observable<Country[]> {
+  searchRegion(region: Region): Observable<Country[]> {
     // TODO
-    const url = `${this.apiURL}/region/${region}`
-    return this.geCountriesRequest(url)
-    .pipe(
-      tap( countries => this.chacheStore.byRegion = { region, countries } ),
-    )
+    const url = `${this.apiURL}/region/${region}`;
+    return this.geCountriesRequest(url).pipe(
+      tap((countries) => (this.chacheStore.byRegion = { region, countries })),
+      tap( () => this.saveToLocalStorage()),
+    );
   }
 }
-
-
